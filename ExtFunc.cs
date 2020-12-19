d390455392ddd08217a17f8c516a2523a044f8ea
Diavolo <es597@sussex.ac.uk>
2020-12-19 22:04:18 +0100

    Clantag PlayerTitle

---
+++ ExtFunc.cs
@@ -0,0 +1,38 @@
+﻿using InfinityScript;
+using System.Text;
+
+namespace LoggingUtil
+{
+    public static class ExtFunc
+    {
+        public static unsafe string GetPlayerTitle(this Entity player)
+        {
+            if (player == null || !player.IsPlayer)
+                return null;
+
+            int address = 0x38A4 * player.EntRef + 0x01AC5548;
+
+            StringBuilder result = new StringBuilder();
+
+            for (; address < address + 8 && *(byte*)address != 0; address++)
+                result.Append(Encoding.ASCII.GetString(new byte[] { *(byte*)address }));
+
+            return result.ToString();
+        }
+
+        public static unsafe string GetClanTag(this Entity player)
+        {
+            if (player == null || !player.IsPlayer)
+                return null;
+
+            int address = 0x38A4 * player.EntRef + 0x01AC5564;
+
+            StringBuilder result = new StringBuilder();
+
+            for (; address < address + 8 && *(byte*)address != 0; address++)
+                result.Append(Encoding.ASCII.GetString(new byte[] { *(byte*)address }));
+
+            return result.ToString();
+        }
+    }
+}
--- LoggingUtil.cs
+++ LoggingUtil.cs
@@ -21,14 +21,14 @@ namespace LoggingUtil
 
             PlayerConnecting += (Entity player) =>
             {
-                customLog.Info("Player {0} connecting with IP: {1} with HWID: {2} with GUID: {3} with XUID: {4}", player.Name, player.IP.Address, player.HWID, player.GUID, player.GetXUID());
+                customLog.Info("Player {0} connecting with IP: {1} with HWID: {2} with GUID: {3} with XUID: {4} with ClanTag: {5} with Title: {6}", player.Name, player.IP.Address, player.HWID, player.GUID, player.GetXUID(), player.GetClanTag(), player.GetPlayerTitle());
 
                 if(sv_HWIDProtect)
                 {
                     if (IPList.Contains(player.IP.Address.ToString()))
                     {
                         Utilities.ExecuteCommand($"dropclient {player.EntRef} \"Same IP, different HWID. Is the cat stepping on the keyboard?\"");
-                        customLog.Info("Player {0} connecting with HWID: {1} was kicked because he has the same IP of another player but different HWID", player.Name, player.HWID);
+                        customLog.Info("Player {0} connecting with IP: {1} with HWID: {2} was kicked because he has the same IP of another player but different HWID", player.Name, player.IP.Address, player.HWID);
                     }
                     else
                         IPList.Add(player.IP.Address.ToString());
@@ -36,7 +36,7 @@ namespace LoggingUtil
                     if(HWIDList.Contains(player.HWID))
                     {
                         Utilities.ExecuteCommand($"dropclient {player.EntRef} \"Same HWID of another online player. Is the cat stepping on the keyboard?\"");
-                        customLog.Info("Player {0} connecting with HWID: {1} was kicked because he has the same HWID of another online player", player.Name, player.HWID);
+                        customLog.Info("Player {0} connecting with IP: {1} with HWID: {2} was kicked because he has the same HWID of another online player", player.Name, player.IP.Address, player.HWID);
                     }
                     else
                         HWIDList.Add(player.HWID);
